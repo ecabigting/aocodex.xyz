5528a38e6d338bebc2bebb46ae2852acef28e0da
ecabigting <ericcabigting@outlook.com>
2021-06-21 22:31:24 +0400

    fix nav

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -3,21 +3,34 @@ import Link from 'next/link'
 export default function Nav(){
     return (
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/cookbook">Cook Book</Link>
-                    </li>
-                    <li>
-                        <Link href="/player">Player</Link>
-                    </li>
-                    <li>
-                        <Link href="/recentbattles">Recent Battles</Link>
-                    </li>
-                </ul>
+            <nav className="flex items-center flex-wrap py-3 px-3">
+                <div className="inline-flex items-center p-2 mr-4 text-xl font-bold hover:text-gray-700">
+                    <Link href="/">AOCodex</Link>
+                </div>
+                <div className='hidden w-full lg:inline-flex lg:flex-grow lg:w-auto'>
+                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                        <Link href='/player'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:bg-gray-400 hover:text-white '>
+                                Player
+                            </a>
+                        </Link>
+                        <Link href='/potions'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:bg-gray-400 hover:text-white'>
+                                Potions
+                            </a>
+                        </Link>
+                        <Link href='/cookbook'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:bg-gray-400 hover:text-white'>
+                                Cook Book
+                            </a>
+                        </Link>
+                        <Link href='/recentbattles'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:bg-gray-400 hover:text-white'>
+                                Recent Battles
+                            </a>
+                        </Link>
+                    </div>
+                </div>
             </nav>
         </div>
     );
